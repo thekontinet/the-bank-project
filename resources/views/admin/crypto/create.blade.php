023e9075663dc159b39c6bcf2ea2e631eccb5f20
rolomtech <support@rolomtech.com>
2023-04-20 23:57:43 +0100

    New partial update

--- resources/views/admin/crypto/create.blade.php
+++ resources/views/admin/crypto/create.blade.php
@@ -8,7 +8,7 @@
             <img class="block w-24 h-24 mx-auto border rounded-full" x-show="wallet" x-bind:src="'https://cryptocompare.com' + wallet.CoinInfo.ImageUrl" alt="logo">
             <input type="hidden" name="image_url" x-bind:value="'https://cryptocompare.com' + wallet.CoinInfo.ImageUrl">
             <div>
-                <select name="name" @click="wallet = cryptos.find(w => w.CoinInfo.FullName === $event.target.value)" class="w-full px-4 py-3 rounded">
+                <select class="form-control" name="name" @click="wallet = cryptos.find(w => w.CoinInfo.FullName === $event.target.value)" class="w-full px-4 py-3 rounded">
                     <option>Select Cryptocurrency</option>
                     <template x-for="crypto in cryptos">
                         <option x-bind:value="crypto.CoinInfo.FullName"><template x-text='crypto.CoinInfo.FullName'></template></option>
