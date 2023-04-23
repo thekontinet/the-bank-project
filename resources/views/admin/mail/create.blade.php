fb1c115b284ee08b413638f99dfe6bdc3ed2479b
rolomtech <support@rolomtech.com>
2023-04-23 03:01:27 +0100

    Added summernote to admin mail message

--- resources/views/admin/mail/create.blade.php
+++ resources/views/admin/mail/create.blade.php
@@ -1,6 +1,6 @@
 <x-admin-layout>
     <header class="my-8">
-        <h4 class="text-3xl font-medium uppercase dark:text-muted-100">Send Mail</h4>
+        <h4 class="text-lg font-medium uppercase dark:text-muted-100">Send Mail</h4>
     </header>
 
     <form class="space-y-4" action="{{route('admin.mail.store')}}" method="POST">
@@ -23,37 +23,27 @@
         </div>
         <div>
             <div class="prose max-w-none">
-                <textarea id="message" name="message" class="hidden w-full"></textarea>
+                <textarea id="summernote" name="message" class="hidden w-full"></textarea>
                 <div id="message-editor" class="bg-gray-100 shadow-sm min-h-[100px] dark:text-muted-100 dark:bg-muted-1000 focus-within:ring-blue-500 focus-within:ring-2">
                 </div>
             </div>
+            {{-- <div id="summernote"></div> --}}
             <x-input-error :messages="$errors->get('message')" class="mt-2" />
         </div>
         <x-primary-button>Send</x-primary-button>
     </form>
 
     @push('styles')
-    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
     @endpush
 
     @push('scripts')
-    <script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
     <script>
-        var messageEditor = new Quill('#message-editor', {
-          theme: 'snow',
-          placeholder: 'Type your message here...',
-          modules: {
-            toolbar: [
-              ['bold', 'italic', 'underline', 'strike'],
-              [{ 'header': [1, 2, 3, 4, 5, 6, false] }]
-            ]
-          }
-        });
-
-        var messageInput = document.querySelector('#message');
-        messageEditor.on('text-change', function(delta, oldDelta, source) {
-          messageInput.value = messageEditor.root.innerHTML;
-        });
-      </script>
+        $('#summernote').summernote({
+            height:400
+        })
+    </script>
     @endpush
 </x-admin-layout>
