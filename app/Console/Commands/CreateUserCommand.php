674109dde5cfdc11d244bdd5e6c6e45a2f25e6ba
rolomtech <support@rolomtech.com>
2023-07-08 03:38:40 +0100

    Added Artisan command to create admin user

---
+++ app/Console/Commands/CreateUserCommand.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
+
+class CreateUserCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:auth-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new user account';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $name = $this->ask('Name');
+        $email = $this->ask('Email');
+
+        while(User::whereEmail($email)->first()){
+            $email = $this->ask('Email');
+        }
+
+        $isAdmin = $this->confirm('Is this an admin account ?');
+        $password = $this->secret('Password');
+        $factory = User::factory();
+
+        $factory->create([
+            'name' => $name,
+            'email' => $email,
+            'password' => Hash::make($password),
+            'is_admin' => $isAdmin
+        ]);
+    }
+}
