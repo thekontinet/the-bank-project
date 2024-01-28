f91bae83d791dfe59616e1320e2e902c3294de47
roland marvelous <thekontinet@gmail.com>
2024-01-28 09:51:12 +0100

    UI fix: copy wallet address button on deposit page

--- app/Http/Controllers/Admin/CryptoWalletController.php
+++ app/Http/Controllers/Admin/CryptoWalletController.php
@@ -10,17 +10,22 @@ use Illuminate\Support\Facades\Http;
 
 class CryptoWalletController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $wallets = Wallet::paginate();
+
         return view('admin.crypto.index', compact('wallets'));
     }
 
-    public function create(){
+    public function create()
+    {
         $cryptos = $this->getCryptoData();
+
         return view('admin.crypto.create', compact('cryptos'));
     }
 
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $data = $request->validate([
             'name' => ['required'],
             'symbol' => ['required'],
@@ -33,17 +38,18 @@ class CryptoWalletController extends Controller
         return back()->with('message', 'Added successfully');
     }
 
-    private function getCryptoData(){
-        $cacheKey = __CLASS__ . __FUNCTION__ . ':cache';
+    private function getCryptoData()
+    {
+        $cacheKey = __CLASS__.__FUNCTION__.':cache';
 
-        if(Cache::has($cacheKey)){
+        if (Cache::has($cacheKey)) {
             return Cache::get($cacheKey);
         }
 
-        $url = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key=' . env('CRYPTO_API_KEY');
+        $url = 'https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&tsym=USD&api_key='.env('CRYPTO_API_KEY');
         $response = Http::get($url);
 
-        if(!$response->successful()){
+        if (!$response->successful()) {
             return abort('400', 'Something went wrong please try again');
         }
 
@@ -53,9 +59,11 @@ class CryptoWalletController extends Controller
         return $cryptos;
     }
 
-    public function destroy($id){
+    public function destroy($id)
+    {
         $wallet = Wallet::findOrFail($id);
         $wallet->delete();
+
         return back()->with('message', 'Deleted');
     }
 }
--- resources/views/deposit/cypto.blade.php
+++ resources/views/deposit/cypto.blade.php
@@ -2,7 +2,7 @@
     <div class="w-full pt-32 pb-20">
         <div class="w-full px-4 mx-auto">
             <!--Form-->
-            <form action="{{route('deposit.store')}}" method="POST"  x-data="{wallets:{{$walletsJSON}}, wallet:null}">
+            <form action="{{ route('deposit.store') }}" method="POST" x-data="{ wallets: {{ $walletsJSON }}, wallet: null }">
                 @csrf
                 <div class="grid w-full gap-8 ltablet:gap-16 lg:gap-16">
                     <!--Content column-->
@@ -25,7 +25,8 @@
                                         <select name="account"
                                             class="w-full px-3 py-2 font-sans text-sm leading-5 placeholder-gray-300 transition-all duration-300 bg-white border rounded-lg appearance-none h-11 border-muted-300 text-muted-600 focus:border-muted-300 focus:shadow-lg dark:placeholder-gray-600 dark:bg-muted-1000 dark:text-muted-200 dark:border-muted-800 dark:focus:border-muted-800 tw-accessibility">
                                             @foreach ($accounts as $account)
-                                                <option value="{{ $account->number }}">{{ ucfirst($account->number) }} - {{ money($account->balance, $account->currency) }}</option>
+                                                <option value="{{ $account->number }}">{{ ucfirst($account->number) }} -
+                                                    {{ money($account->balance, $account->currency) }}</option>
                                             @endforeach
                                         </select>
                                         <div
@@ -53,8 +54,8 @@
                                     </div>
                                 </div>
 
-                                 <!--Select-->
-                                 <div class="pt-8">
+                                <!--Select-->
+                                <div class="pt-8">
                                     <div class="relative group">
                                         <select name="wallet"
                                             class="w-full px-3 py-2 font-sans text-sm leading-5 placeholder-gray-300 transition-all duration-300 bg-white border rounded-lg appearance-none h-11 border-muted-300 text-muted-600 focus:border-muted-300 focus:shadow-lg dark:placeholder-gray-600 dark:bg-muted-1000 dark:text-muted-200 dark:border-muted-800 dark:focus:border-muted-800 tw-accessibility"
@@ -70,27 +71,35 @@
                                         </div>
                                     </div>
                                     <x-input-error :messages="$errors->get('wallet')" class="mt-2" />
-                                 </div>
+                                </div>
 
                                 <!--Payment Info-->
-                                <div class="flex flex-col items-center justify-center px-4 py-2 my-6 mt-8 border rounded-lg" x-show='wallet'>
-                                    <img class="object-cover w-12 h-12 rounded-full" :src="wallet.image_url" :alt="wallet.name"/>
+                                <div class="flex flex-col items-center justify-center px-4 py-2 my-6 mt-8 border rounded-lg"
+                                    x-show='wallet'>
+                                    <img class="object-cover w-12 h-12 rounded-full" :src="wallet.image_url"
+                                        :alt="wallet.name" />
                                     <h4 class="text-base" x-text="wallet.name"></h4>
-                                    <div class="flex items-center justify-between w-full max-w-md mt-5 gap-x-2">
-                                        <input type="text" x-bind:value="wallet.address" readonly class="flex-1 block h-10 px-4 text-sm text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
+                                    <div class="flex items-center w-full max-w-md mt-5 gap-x-2">
+                                        <input type="text" x-bind:value="wallet.address" readonly
+                                            class="flex-1 block h-10 px-4 text-sm text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring" />
 
-                                        <button type="button" @click="navigator.clipboard.writeText(wallet.address); alert('copied')" class="rounded-md hidden sm:block p-1.5 text-gray-700 bg-white border border-gray-200 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring transition-colors duration-300 hover:text-blue-500 dark:hover:text-blue-500">
+                                        <button type="button"
+                                            @click="navigator.clipboard.writeText(wallet.address); alert('copied')"
+                                            class="rounded-md p-1.5 text-gray-700 bg-white border border-gray-200 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring transition-colors duration-300 hover:text-blue-500 dark:hover:text-blue-500">
                                             <i class="w-5 h-5 iconify" data-icon="lucide:copy"></i>
                                         </button>
                                     </div>
-                                    <p class="max-w-md p-3 mx-auto mt-4 text-center text-green-500 bg-green-100 rounded-md">
-                                        Copy and make deposit to the above wallet address. Funds are automatically credited immediately after transaction is verified by the blockchain.
+                                    <p
+                                        class="max-w-md p-3 mx-auto mt-4 text-center text-green-500 bg-green-100 rounded-md">
+                                        Copy and make deposit to the above wallet address. Funds are automatically
+                                        credited immediately after transaction is verified by the blockchain.
                                     </p>
                                 </div>
 
                                 <!--Buttons-->
                                 <div class="flex gap-4 py-8">
-                                    <x-primary-button x-bind:disabled="!wallet">I have made payment</x-primary-button>
+                                    <x-primary-button x-bind:disabled="!wallet">I have made
+                                        payment</x-primary-button>
                                 </div>
                             </div>
                         </div>
