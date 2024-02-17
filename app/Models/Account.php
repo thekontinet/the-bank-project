58d7f7554ae33e433404b3a790156d807fb9f96b
roland marvelous <thekontinet@gmail.com>
2024-02-17 11:25:37 +0100

    fixed credit issue

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use App\Exceptions\TransactionException;
-use Exception;
+use Brick\Math\BigInteger;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
@@ -12,51 +12,55 @@ class Account extends Model
 {
     use HasFactory;
 
-    const TYPE_SAVINGS = 'savings';
-    const TYPE_CURRENT= 'current';
-    const TYPE_JOINT = 'coporate';
+    public const TYPE_SAVINGS = 'savings';
+    public const TYPE_CURRENT = 'current';
+    public const TYPE_JOINT = 'coporate';
 
-    const TYPES = [self::TYPE_SAVINGS, self::TYPE_CURRENT, self::TYPE_JOINT];
+    public const TYPES = [self::TYPE_SAVINGS, self::TYPE_CURRENT, self::TYPE_JOINT];
 
     protected $guarded = [];
 
-    public function getRouteKeyName(){
+    public function getRouteKeyName()
+    {
         return 'number';
     }
 
-    public function getShortNumberAttribute(){
-        return "xxx xx " . substr($this->number, 6);
+    public function getShortNumberAttribute()
+    {
+        return 'xxx xx '.substr($this->number, 6);
     }
 
-
-    public static function make(User $user, $type, $currency = 'USD', $joint = false){
-        try{
+    public static function make(User $user, $type, $currency = 'USD', $joint = false)
+    {
+        try {
             DB::beginTransaction();
-            $account =  static::create([
+            $account = static::create([
                 'user_id' => $user->id,
                 'name' => $user->name,
                 'type' => $type,
                 'number' => self::generateAccountNumber(),
                 'currency' => $currency,
-                'is_joint' => !!$joint
+                'is_joint' => (bool) $joint,
             ]);
             DB::commit();
+
             return $account;
-        }catch(Exception $e){
+        } catch (\Exception $e) {
             DB::rollBack();
             throw $e;
         }
     }
 
-    public static function generateAccountNumber(){
+    public static function generateAccountNumber()
+    {
         $accountNumber = null;
 
-        while(!$accountNumber){
+        while (!$accountNumber) {
             $number = mt_rand(1000000000, 9999999999);
 
             $exists = Account::where('number', $number)->exists();
 
-            if(!$exists){
+            if (!$exists) {
                 $accountNumber = $number;
             }
         }
@@ -64,75 +68,85 @@ class Account extends Model
         return $accountNumber;
     }
 
-
-    public function removeHolders(){
+    public function removeHolders()
+    {
         $this->holders()->where('user_id', '<>', $this->user_id)->delete();
     }
 
-    public function credit(int $amount){
-        $amount = abs($amount);
-
-        if($amount <= 0){
+    public function credit(int $amount)
+    {
+        if ($amount <= 0) {
             throw new TransactionException('Invalid amount specified');
         }
 
-        $this->balance += $amount * 100;
-        return $this->save();
+        $amount = BigInteger::of($amount);
+
+        $newBalance = $amount->multipliedBy(100)->plus($this->balance);
+
+        return $this->update(['balance' => (string) $newBalance]);
     }
 
-    public function debit(int $amount){
+    public function debit(int $amount)
+    {
         $amount = abs($amount);
 
-        if($amount <= 0){
+        if ($amount <= 0) {
             throw new TransactionException('Invalid amount specified');
         }
 
-        if($amount > $this->balance){
+        if ($amount > $this->balance) {
             throw new TransactionException('Insufficient balance');
         }
 
         $this->balance -= $amount * 100;
+
         return $this->save();
     }
 
-    public function hasToken(){
+    public function hasToken()
+    {
         return $this->tokens()->exists();
     }
 
-    public function isJoint(){
+    public function isJoint()
+    {
         return $this->getAttribute('is_joint');
     }
 
-    public function transactions(){
+    public function transactions()
+    {
         return $this->hasMany(Transaction::class);
     }
 
-    public function tokens(){
+    public function tokens()
+    {
         return $this->morphMany(Token::class, 'tokenable');
     }
 
-    public function user(){
+    public function user()
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function holders(){
+    public function holders()
+    {
         return $this->belongsToMany(User::class, 'account_holders');
     }
 
     protected static function booted(): void
     {
-        static::addGlobalScope(function($q){
-            /**@var App\Model\User $user */
+        static::addGlobalScope(function ($q) {
+            /** @var App\Model\User $user */
             $user = auth()->user();
-            if($user && !$user->hasAdminRole()){
+            if ($user && !$user->hasAdminRole()) {
                 return $q->where('accounts.user_id', $user->id);
             }
         });
 
-        static::created(function(Account $account){
-            try{
+        static::created(function (Account $account) {
+            try {
                 $account->holders()->syncWithoutDetaching($account->user_id);
-            }catch(Exception $e){
+            } catch (\Exception $e) {
                 $account->delete();
             }
         });
