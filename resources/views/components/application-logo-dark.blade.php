23910308400a676d9979411cc506b211e7696691
roland marvelous <thekontinet@gmail.com>
2024-02-18 18:25:25 +0100

    Making logo name from .env optional

--- resources/views/components/application-logo-dark.blade.php
+++ resources/views/components/application-logo-dark.blade.php
@@ -17,6 +17,5 @@
   </a> --}}
 <a href="/" style="display: inline-flex; align-items: center; gap:4">
     <img width="40px" src="/images/logo.png" alt="image" onerror="this.style.display = 'none'">
-    <h4 style="font-weight: 800; font-size: 18px">{{config('app.name')}}</h4>
+    <h4 style="font-weight: 800; font-size: 18px">{{ env('APP_LOGO_NAME') ?? env('APP_NAME') }}</h4>
 </a>
-
--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <a href="/" style="display: inline-flex; align-items: center; gap:4">
     <img width="40px" src="/images/logo.png" alt="image" onerror="this.style.display = 'none'">
-    <h4 style="font-weight: 800; font-size: 18px; color:white">{{config('app.name')}}</h4>
+    <h4 style="font-weight: 800; font-size: 18px; color:white">{{ env('APP_LOGO_NAME') ?? env('APP_NAME') }}</h4>
 </a>
