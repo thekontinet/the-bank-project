5377e43d4fcfdcec8079f1a5c3b48db3e3e7e722
roland marvelous <thekontinet@gmail.com>
2024-05-10 17:28:37 +0100

    adjusted balance font

--- resources/views/components/account-card.blade.php
+++ resources/views/components/account-card.blade.php
@@ -1,25 +1,26 @@
 @props(['account', 'href' => null, 'hasAvatar' => false])
 
-<div class="py-5 px-4 flex rounded-lg text-white bg-gradient-to-tr from-primary-400 to-primary-600 dark:from-slate-900 dark:to-slate-600 w-full">
+<div
+    class="py-5 px-4 flex rounded-lg text-white bg-gradient-to-tr from-primary-400 to-primary-600 dark:from-slate-900 dark:to-slate-600 w-full">
     <div class="flex-1">
         <div class="flex items-center gap-2">
             @if ($hasAvatar)
-                <img class="object-cover mx-0 border-4 border-primary-300 rounded-full w-10 h-10" src="{{$account->user->avatar}}" alt="User photo" width="40" height="40">
+                <img class="object-cover mx-0 border-4 border-primary-300 rounded-full w-10 h-10"
+                    src="{{ $account->user->avatar }}" alt="User photo" width="40" height="40">
             @endif
             <div>
-                <h4>{{ucfirst($account->type)}} Account</h4>
-                <p class="font-mono text-sm">{{$account->number}}</p>
+                <h4>{{ ucfirst($account->type) }} Account</h4>
+                <p class="font-mono text-sm">{{ $account->number }}</p>
             </div>
         </div>
-        <p class="font-medium font-mono text-3xl my-2 tracking-wider">@money($account->balance, $account->currency)</p>
-        <p class="text-sm text-primary-200 font-semibold">{{$account->name}}</p>
+        <p class="font-bold font-mono text-2xl my-2 tracking-tighter">@money($account->balance, $account->currency)</p>
+        <p class="text-sm text-primary-200 font-semibold">{{ $account->name }}</p>
     </div>
     @if ($href)
-    <div class="text-right">
-        <a class="bg-primary-200 flex items-center justify-center rounded-full p-1 text-primary-600" href="{{$href}}"><span class="iconify w-6 h-6 ml-auto" data-icon="lucide:arrow-up-right"></span></a>
-    </div>
+        <div class="text-right">
+            <a class="bg-primary-200 flex items-center justify-center rounded-full p-1 text-primary-600"
+                href="{{ $href }}"><span class="iconify w-6 h-6 ml-auto"
+                    data-icon="lucide:arrow-up-right"></span></a>
+        </div>
     @endif
 </div>
-
-
-
