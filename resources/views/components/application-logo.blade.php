1140e73b603298e8e53e00c4ac78b3e1e60decff
roland marvelous <thekontinet@gmail.com>
2024-06-11 12:48:09 +0100

    new minor change

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,4 +1,4 @@
 <a href="/" style="display: inline-flex; align-items: center; gap:4">
-    <img style="width:100px; height:100px; object-fit: contain" src="/images/logo.png" alt="image" onerror="this.style.display = 'none'">
+    <img style="width:100px; height:50px; object-fit: contain" src="/images/logo.png" alt="image" onerror="this.style.display = 'none'">
     <h4 style="font-weight: 800; font-size: 18px; color:white">{{ env('APP_LOGO_NAME') ?? env('APP_NAME') }}</h4>
 </a>
