e3cd26c364a3a7b6fe1cd5200d1f81afcc46cfb9
= <thekontinet@gmail.com>
2026-03-14 01:36:19 +0100

    Refactor code structure for improved readability and maintainability

--- resources/views/components/application-logo-dark.blade.php
+++ resources/views/components/application-logo-dark.blade.php
@@ -1,4 +1,4 @@
 <a href="/" class="flex items-center gap-2 h-8 overflow-hidden" style="display: flex; align-items: center; gap:4">
   <img class="flex-1 h-full object-center object-contain" src="https://horizoncitiintl.com/images/logo.png" alt="image" onerror="this.style.display = 'none'">
-  <h4 style="font-weight: 800; font-size: 18px; color:white">{{ env('APP_LOGO_NAME') ?? env('APP_NAME') }}</h4>
+  <h4 style="font-weight: 800; font-size: 18px" class="dark:text-white"> {{ env('APP_LOGO_NAME') ?? env('APP_NAME') }} </h4>
 </a>
